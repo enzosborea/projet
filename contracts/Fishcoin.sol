d8237c9a41a962c070940c23bee9eaf2d79b3b0a
enzo sborea <enzo.sborea@outlook.com>
2022-01-21 15:18:17 +0100

    gvgvgv
    vgvgv

---
+++ contracts/Fishcoin.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Fishcoin is ERC20 {
+    address public admin;
+    constructor() ERC20("Fishcoin", "FC") {
+        _mint(msg.sender, 10000 * 10 ** 18);
+        admin = msg.sender;
+    }
+
+    function mint(address to, uint amount) external {
+        require(msg.sender == admin, 'only admin');
+        _mint(to, amount);
+    }
+
+    function burn(uint amount) external {
+        _burn(msg.sender, amount);
+    }
+
+
+}
+
+
+
+
